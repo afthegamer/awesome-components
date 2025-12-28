0a96487ec50dd60a6e639116a97f92e46832459a
Brad Dos Santos Patatas <brad.dossantospatatas@acedigitale.com>
2025-12-28 20:25:09 +0100

    chore: update post-list-item layout with Material Design and add reusable comments component

--- src/app/features/social-media/components/post-list-item/post-list-item.ts
+++ src/app/features/social-media/components/post-list-item/post-list-item.ts
@@ -1,10 +1,27 @@
 import { Component, input } from '@angular/core';
 import { Post } from '../../models/post.model';
-import { TitleCasePipe } from '@angular/common';
+import { DatePipe, TitleCasePipe } from '@angular/common';
+import {
+  MatCard,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage,
+  MatCardSubtitle,
+  MatCardTitle,
+} from '@angular/material/card';
 
 @Component({
   selector: 'app-post-list-item',
-  imports: [TitleCasePipe],
+  imports: [
+    TitleCasePipe,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardTitle,
+    MatCardSubtitle,
+    DatePipe,
+    MatCardImage,
+  ],
   templateUrl: './post-list-item.html',
   styleUrl: './post-list-item.scss',
 })
