afa5bb5f745c2d79f0f310ab271711582d86782c
Brad Dos Santos Patatas <brad.dossantospatatas@acedigitale.com>
2026-01-03 02:39:47 +0100

    chore: update comments UI with hover animations and entry transitions, remove unused animation providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,7 +10,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     // ...CORE_PROVIDERS,
     provideHttpClient(),
-    provideAnimations(),
-    provideAnimationsAsync(),
+    // provideAnimations(),
+    // provideAnimationsAsync(),
   ],
 };
